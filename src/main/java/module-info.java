80710012660f28d1381c9937c4b067d91f9bc4ee
Himeshika <himeshikanethmini@gmail.com>
2025-09-25 14:26:57 +0530

    project finalized

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,22 +1,22 @@
 module orm.coursework.himeshika {
-    // Hibernate / JPA
+
     requires jakarta.persistence;
     requires org.hibernate.orm.core;
 
-    // Lombok (compile-time only)
+
     requires static lombok;
 
-    // JavaFX
+
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
 
-    // Other libs
+
     requires bcrypt;
     requires java.naming;
 
-    // Export packages (accessible to other modules)
+
     exports edu.ijse.entity;
     exports edu.ijse.dto;
     exports edu.ijse.DAO;
@@ -25,10 +25,9 @@ module orm.coursework.himeshika {
     exports edu.ijse.config;
     exports edu.ijse.controller;
 
-    // For JavaFX table models
+
     opens edu.ijse.tm to javafx.base;
 
-    // Open packages (needed for reflection by Hibernate / JavaFX)
     opens edu.ijse.entity to org.hibernate.orm.core, jakarta.persistence;
     opens edu.ijse to javafx.fxml, javafx.graphics;
     opens edu.ijse.controller to javafx.fxml;
